8ed9791ba715210978f666fdf1c650699ea22180
rituraj00 <rituraj171926@gmail.com>
2023-02-09 17:34:02 +0530

    A simple CRUD GO api uses local variable DB of books

---
+++ main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"errors"
+)
+
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+var books = []book{
+	{ID: "1", Title: "Hints for Self Culture", Author: "Lala Har Dayal", Quantity: 1},
+	{ID: "2", Title: "Six Easy Pieces", Author: "Richard Feynman", Quantity: 2},
+	{ID: "3", Title: "Doglapan", Author: "Ashneer Grover", Quantity: 3},
+}
+
+func getBooks(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, books)
+}
+
+func createBook(c *gin.Context) {
+	var newBook book
+
+	if err := c.BindJSON(&newBook); err != nil {
+		return
+	}
+
+	books = append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+}
+
+func getBookById(id string) (*book, error) {
+	for i, b := range books {
+		if b.ID == id {
+			return &books[i], nil
+		}
+	}
+	return nil, errors.New("book not found")
+}
+
+func bookById(c *gin.Context) {
+	id := c.Param("id")
+	book, err := getBookById(id)
+
+	if err != nil {
+		return
+	}
+
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func checkoutBook(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing query paramters"})
+	}
+
+	book, err := getBookById(id)
+
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Book not found"})
+	}
+
+	if book.Quantity <= 0 {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Books out of stock"})
+	}
+
+	book.Quantity -= 1
+
+	c.IndentedJSON(http.StatusOK, book)
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
+	router.GET("/books/:id", bookById)
+	router.PATCH("/checkout/:id", checkoutBook)
+	router.Run("localhost:8080")
+}
